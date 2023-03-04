ac2da89e83394d7029972c5a8a4d7a70ef1147e8
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2023-03-04 14:09:47 -0600

    doing research for seats

--- src/main/java/com/aviato/demo/models/Flights.java
+++ src/main/java/com/aviato/demo/models/Flights.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,12 +32,30 @@ public class Flights {
     @Column(name = "arrivalTime", nullable = false)
     private LocalDateTime arrivalTime;
 
+    // Enumerated(EnumType.STRING) used to indicate that an enum type attribute should be persisted as a string value in the database.
+    /* RESEARCH FOR SEATS
+
+    https://stackoverflow.com/questions/60895270/airplane-seating-program-2-dimensional-array
+    * */
+
+    //Enum values are economy or first (class).  It just limits the options.
+    @Enumerated(EnumType.STRING)
     @Column(name = "seatType", nullable = false, length = 15)
     private String seatType;
-
     @Column(name = "seatAvailable", nullable = false)
     private boolean seatAvailable;
 
+//
+//    https://github.com/vishal1605/Movie-seat-project/blob/main/src/main/java/com/bus/beans/Customer.java
+    @OneToMany(mappedBy = "flight", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Seat> seats = new ArrayList<>();
+
+
+//    LAZY - To load it on-demand (i.e. lazily) when you call
+//    EAGER - To load it together with the rest of the fields (i.e. eagerly)
+//    If flight has many passengers it is not efficient to load all of its students together with it,
+//    especially when they are not needed and in suchlike cases you can declare that you want students to be loaded when they are actually needed.
+//    This is called lazy loading.
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
@@ -46,8 +65,8 @@ public class Flights {
             joinColumns = @JoinColumn(name = "flight_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id"))
     //would have to use a set in a many-to-many relationship
-
     private Set<User> users = new HashSet<>();
+
 
     public Flights() {
     }
