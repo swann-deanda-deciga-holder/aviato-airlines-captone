a29d90634bd88cfe6a07a0508737ccb926f20b37
Herb <herbrholder@gmail.com>
2023-03-08 11:00:49 -0600

    resolved conflicts

---
+++ src/main/java/com/aviato/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.aviato.demo.repositories;
+
+import com.aviato.demo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
