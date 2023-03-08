78b9d031e7b6cb36320f224a8a631a0362696fc0
Herb <herbrholder@gmail.com>
2023-03-08 11:20:23 -0600

    Resolved ALL Conflicts Working version

--- src/main/java/com/aviato/demo/controllers/LoginController.java
+++ src/main/java/com/aviato/demo/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.aviato.demo.controllers;
 
 import org.springframework.ui.Model;
 import com.aviato.demo.models.User;
+import com.aviato.demo.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
