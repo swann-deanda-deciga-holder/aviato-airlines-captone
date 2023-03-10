bed2407e9a114f7f3f53ffba031f14aa6700c0b3
Herb <herbrholder@gmail.com>
2023-03-10 14:06:58 -0600

    rm: src

--- src/main/java/com/aviato/demo/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aviato.demo.repositories;
-
-import com.aviato.demo.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
