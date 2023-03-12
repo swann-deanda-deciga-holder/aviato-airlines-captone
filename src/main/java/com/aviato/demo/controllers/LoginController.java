8dee4e2cb9f909b67c59335549346a4c53a2546c
Steve <stevedee97@gmail.com>
2023-03-12 11:17:49 -0500

    adding comments to code

--- src/main/java/com/aviato/demo/controllers/LoginController.java
+++ src/main/java/com/aviato/demo/controllers/LoginController.java
@@ -12,11 +12,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginController {
 
+
+    // Declared an instance variable of type UserRepository //
     private final UserRepository userRepository;
 
+    // Constructor that accepts a UserRepository object and assigns it to the instance variable to access methods //
     public LoginController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    // Method that maps to the "/login" URL and returns the "login" view //
 
     @GetMapping("/login")
     public String showLoginForm() {
@@ -28,16 +33,23 @@ public class LoginController {
 
     // Form Validation code, not sure if it works. //
 
+    // Method that handles form submission when the user logs in //
+
     @PostMapping("/login")
     public String login(@ModelAttribute("user") User user, Model model) {
+
+        // Retrieve a User object with the same email address as the submitted form data from the UserRepository//
         User existingUser = userRepository.findByEmail(user.getEmail());
 
 
-
+        // If a User object is not found, add an error message to the model and return the "login" view //
         if (existingUser == null) {
             model.addAttribute("error", "Invalid email or password");
             return "login";
         }
+
+        // If a User object is found, redirect to the home page
+
         return "redirect:/";
     }
 }
--- src/main/java/com/aviato/demo/controllers/ProfileController.java
+++ src/main/java/com/aviato/demo/controllers/ProfileController.java
@@ -14,36 +14,48 @@ import java.security.Principal;
 @Controller
 public class ProfileController {
 
+
+    // Injecting instance //
     @Autowired
     private UserRepository userRepository;
 
+    // Method that maps to the "/profile" URL and returns the "profile" view //
+
     @GetMapping("/profile")
     public String userProfile(Model model) {
+
+        // Retrieve the logged-in User object from the security context //
         User loggedinUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // Add the logged-in User object to the model //
         model.addAttribute("user", userRepository.findById(loggedinUser.getId()).get());
+
+        // Return the "profile" view //
         return "profile";
     }
 
+    // Method that handles form submission when the user edits their profile //
     @PostMapping("/profile/edit")
     public String editProfile(@ModelAttribute User user) {
+
+        // Save the updated User object to the UserRepository //
         userRepository.save(user);
+
+        // Redirect to the "/profile" URL //
         return "redirect:/profile";
     }
 
-
-    // DELETE PROFILE FUNCTIONALITY //
-
-//    @GetMapping("/profile/delete")
-//    public String deleteProfile(Model model) {
-//        User loggedinUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        model.addAttribute("user", userRepository.findById(loggedinUser.getId()).get());
-//        return "delete-profile";
-//    }
-
+    // Method that handles form submission when the user deletes their profile //
     @PostMapping("/profile/delete")
     public String confirmDeleteProfile() {
+
+        // Retrieve the logged-in User object from the security context //
         User loggedinUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // Delete the logged-in User object from the UserRepository //
         userRepository.deleteById(loggedinUser.getId());
+
+        // Redirect to the "/login" URL //
         return "redirect:/login";
     }
 
