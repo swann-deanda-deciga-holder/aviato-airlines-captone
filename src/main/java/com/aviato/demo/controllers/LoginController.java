ffa56b58ccb31a93569ba06c0aac76bd4a4d4c7f
Steve <stevedee97@gmail.com>
2023-03-13 10:37:08 -0500

    commit before pull

--- src/main/java/com/aviato/demo/controllers/LoginController.java
+++ src/main/java/com/aviato/demo/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package com.aviato.demo.controllers;
 
 import org.apache.tomcat.util.modeler.BaseAttributeFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import com.aviato.demo.models.User;
 import com.aviato.demo.repositories.UserRepository;
@@ -15,6 +17,9 @@ public class LoginController {
 
     // Declared an instance variable of type UserRepository //
     private final UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     // Constructor that accepts a UserRepository object and assigns it to the instance variable to access methods //
     public LoginController(UserRepository userRepository) {
@@ -37,19 +42,16 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(@ModelAttribute("user") User user, Model model) {
-
-        // Retrieve a User object with the same email address as the submitted form data from the UserRepository//
         User existingUser = userRepository.findByEmail(user.getEmail());
-
-
-        // If a User object is not found, add an error message to the model and return the "login" view //
         if (existingUser == null) {
             model.addAttribute("error", "Invalid email or password");
             return "login";
         }
-
-        // If a User object is found, redirect to the home page
-
+        if (!passwordEncoder.matches(user.getPassword(), existingUser.getPassword())) {
+            model.addAttribute("error", "Invalid email or password");
+            return "login";
+        }
         return "redirect:/";
     }
+
 }
